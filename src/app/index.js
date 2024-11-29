cf34eaaf51f150d36eb4ca0d872f4d5a7e6b55b6
Ryan Godoi da Silva <ryan.godoi.dasilva@gmail.com>
2024-11-29 14:44:35 -0300

    modificando aplicativo por completo

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+/*
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, FlatList, TouchableOpacity, Modal, TextInput } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Notifications from 'expo-notifications';
+import { Audio } from 'expo-av';
+*/
+import { View, Text } from 'react-native';
+
+export default function app() {
+    return (
+        <View>
+            <Text>
+                olá mundo
+            </Text>
+        </View>
+    );
+}
